f65abecc0c297683b23944dc47a174ed6a220380
Inaldo Ramos Ribeiro <inaldor@bibimoney.com>
2019-06-10 12:14:32 +0100

    Initial Commit

---
+++ BaseProjectProgramactly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseProjectProgramactly
+//
+//  Created by Inaldo Ramos Ribeiro on 10/06/2019.
+//  Copyright © 2019 Inaldo's Software Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
